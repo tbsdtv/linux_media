e8165880285ab58aae14ede79d8f67e40f10997b
CrazyCat <crazycat69@narod.ru>
2020-05-13 23:53:58 +0300

    dvb-core: Minor fix on init.

--- drivers/media/dvb-core/dvb_demux.c
+++ drivers/media/dvb-core/dvb_demux.c
@@ -2047,7 +2047,7 @@ static int dmx_section_feed_start_filtering(struct dmx_section_feed *feed)
 	dvbdmxfeed->feed.sec.secbuf = dvbdmxfeed->feed.sec.secbuf_base;
 	dvbdmxfeed->feed.sec.secbufp = 0;
 	dvbdmxfeed->feed.sec.seclen = 0;
-	dvbdmxfeed->pusi_seen = false;
+	dvbdmxfeed->pusi_seen[0] = false;
 
 	if (!dvbdmx->start_feed) {
 		mutex_unlock(&dvbdmx->mutex);
